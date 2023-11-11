7175f55573f65b4b5e2218324c3897143302709d
BoomPotato <grassmallow@gmail.com>
2023-11-11 18:50:37 +0800

    added comment for unused bunker img code

--- asset-controller.js
+++ asset-controller.js
@@ -48,14 +48,6 @@ function getOctoImg2() {
   return img;
 }
 
-function getBunkerImg() {
-  let img = document.createElement("img");
-  img.setAttribute("src", "./assets/bunker.png");
-  img.setAttribute("height", "30");
-  img.setAttribute("width", "120");
-  return img;
-}
-
 function getTankImg() {
   let img = document.createElement("img");
   img.setAttribute("src", "./assets/tank.png");
@@ -69,5 +61,14 @@ function getBulletStraightImg() {
   img.setAttribute("src", "./assets/bullet-straight.png");
   img.setAttribute("height", "15");
   img.setAttribute("width", "10");
+  return img;
+}
+
+// Not used in new code, but used by old code in "references.js"
+function getBunkerImg() {
+  let img = document.createElement("img");
+  img.setAttribute("src", "./assets/bunker.png");
+  img.setAttribute("height", "30");
+  img.setAttribute("width", "120");
   return img;
 }
